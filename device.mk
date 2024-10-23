be2a308d93ee0ee931875d2f410fd11bea85647e
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-23 20:51:28 +0530

    houji: Define kernel dir
    This will be picked up by the common tree
    
    Change-Id: Iba50e53cffc1b47d934a528ef4b0a8b553e8d602

--- device.mk
+++ device.mk
@@ -5,4 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 $(call inherit-product, vendor/xiaomi/houji/houji-vendor.mk)
+
+TARGET_KERNEL_DIR := device/xiaomi/houji-kernel
+
 $(call inherit-product, device/xiaomi/sm8650-common/pineapple.mk)
