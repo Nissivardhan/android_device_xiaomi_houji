95c77592e7005c04a48bd06b3ee096783077554b
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-23 21:01:14 +0530

    houji: Namespace the tree
    Change-Id: I9b5d0282a6a407ebbb43d949d2c2e6c12188a0a8

--- device.mk
+++ device.mk
@@ -9,3 +9,7 @@ $(call inherit-product, vendor/xiaomi/houji/houji-vendor.mk)
 TARGET_KERNEL_DIR := device/xiaomi/houji-kernel
 
 $(call inherit-product, device/xiaomi/sm8650-common/pineapple.mk)
+
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
