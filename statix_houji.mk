4c7b48a12a80d6ab90702d23f381d63a088d44b6
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-23 20:34:29 +0530

    houji: Include some needed makefiles
    whoopsie
    
    Change-Id: I492bc3fd6386eea6a97ccd2c29eb86b2478e0297

--- statix_houji.mk
+++ statix_houji.mk
@@ -3,9 +3,14 @@
 # Copyright (c) 2024 StatiXOS
 #
 # SPDX-License-Identifier: Apache-2.0
-# 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/houji/device.mk)
+
+# Inherit some common StatiX stuff.
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
 
 PRODUCT_NAME := statix_houji
 PRODUCT_DEVICE := houji
